8d308d2ec3288c81fec7147383790d5862d81d19
SwareJonge <41187958+SwareJonge@users.noreply.github.com>
2019-02-07 23:03:22 +0100

    Update Version.cpp

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -13,7 +13,7 @@
 	#define BUILD_TYPE_STR ""
 #endif
 
-const std::string scm_rev_str = "Dolphin 5.0"
+const std::string scm_rev_str = "Dolphin 5.0 [LUA Core]"
 #if !SCM_IS_MASTER
 	"" SCM_BRANCH_STR ""
 #endif
