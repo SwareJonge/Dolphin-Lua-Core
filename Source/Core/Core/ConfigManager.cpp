ec9242373109eecdca1394d2ef284ce63cafd854
SwareJonge <41187958+SwareJonge@users.noreply.github.com>
2019-05-21 22:32:31 +0200

    Fixed an issue where it wouldn't save your choise for when you've enabled InfoDisplay

--- Source/Core/Core/ConfigManager.cpp
+++ Source/Core/Core/ConfigManager.cpp
@@ -283,6 +283,7 @@ void SConfig::SaveMovieSettings(IniFile& ini)
 	movie->Set("DumpFrames", m_DumpFrames);
 	movie->Set("DumpFramesSilent", m_DumpFramesSilent);
 	movie->Set("ShowInputDisplay", m_ShowInputDisplay);
+	movie->Set("ShowInfoDisplay", m_ShowRAMDisplay);
 }
 
 void SConfig::SaveDSPSettings(IniFile& ini)
@@ -558,6 +559,7 @@ void SConfig::LoadMovieSettings(IniFile& ini)
 	movie->Get("DumpFrames", &m_DumpFrames, false);
 	movie->Get("DumpFramesSilent", &m_DumpFramesSilent, false);
 	movie->Get("ShowInputDisplay", &m_ShowInputDisplay, false);
+	movie->Get("ShowInfoDisplay", &m_ShowRAMDisplay, false);
 }
 
 void SConfig::LoadDSPSettings(IniFile& ini)
